24aaa6f81ba37e00d7015df196377185ab8a87f9
leena0203 <leenasarode02@gmail.com>
2020-10-19 22:28:26 +0530

    Added: test method to check file all cycle

---
+++ src/test/java/com/EmployeePayroll/FileUtils.java
@@ -0,0 +1,16 @@
+package com.EmployeePayroll;
+
+import java.io.File;
+
+public class FileUtils {
+	public static boolean deleteFile(File contentsToDelete) {
+		File[] allContents = contentsToDelete.listFiles();
+		if (allContents != null) {
+			for (File file : allContents) {
+				deleteFile(file);
+			}
+		}
+		return contentsToDelete.delete();
+	}
+
+}
