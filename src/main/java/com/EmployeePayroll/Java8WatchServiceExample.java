7c0ca3452360550dea0d57733a1efdd64110f69a
leena0203 <leenasarode02@gmail.com>
2020-10-21 18:46:19 +0530

    Added:Watch service class added to watch all activities of file

---
+++ src/main/java/com/EmployeePayroll/Java8WatchServiceExample.java
@@ -0,0 +1,104 @@
+package com.EmployeePayroll;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Java8WatchServiceExample {
+	private final WatchService watcher ;
+	private final Map<WatchKey, Path> dirWatchers;
+
+	/**
+	 * Constructor
+	 * @param dir
+	 * @throws IOException
+	 */
+	Java8WatchServiceExample(Path dir) throws IOException{
+		this.watcher = FileSystems.getDefault().newWatchService();
+		this.dirWatchers = new HashMap<WatchKey, Path>();
+		scanAndRegisterDirectories(dir);
+	}
+
+	/**
+	 * Register given directory and all its sub directories with watchservice
+	 * @param start
+	 * @throws IOException
+	 */
+	private void scanAndRegisterDirectories(final Path start)throws IOException {
+		Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+					throws IOException{
+				registerDirWatcherrs(dir);
+				return FileVisitResult.CONTINUE;
+			}
+		});
+
+	}
+
+	/**
+	 * Register given directory eith watch service
+	 * @param dir
+	 * @throws IOException
+	 */
+	private void registerDirWatcherrs(Path dir) throws IOException{
+		WatchKey key = dir.register(watcher,  StandardWatchEventKinds.ENTRY_CREATE,
+				StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+		dirWatchers.put(key, dir);
+	}
+
+	/**
+	 * Process all events
+	 */
+	void processEvents() {
+		while (true) {
+			WatchKey key;
+			try {
+				key = watcher.take();
+			}catch (InterruptedException x) {
+				return;
+			}
+			Path dir = dirWatchers.get(key);
+			if (dir == null)continue;
+			for(WatchEvent<?> event : key.pollEvents()) {
+				WatchEvent.Kind kind = event.kind();
+
+				Path name = ((WatchEvent<Path>) event).context();
+				Path child = dir.resolve(name);
+				System.out.format("%s: %s\n", event.kind().name(), child);
+
+				/**
+				 * if direcctory created then register it and its sub directories
+				 */
+				if(kind == StandardWatchEventKinds.ENTRY_CREATE) {
+					try {
+						if(Files.isDirectory(child)) {
+							scanAndRegisterDirectories(child);
+						}
+					}catch(IOException x) {
+
+					}
+				}
+			}
+
+			/**
+			 * reset key and remove from set if directory no longer
+			 */
+			boolean valid = key.reset();
+			if(!valid) {
+				dirWatchers.remove(key);
+				if(dirWatchers.isEmpty())
+					break;
+			}
+		}
+	}
+}
